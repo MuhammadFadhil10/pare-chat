ba92f2beef1d1ff0ff3d395ab37bf7ca220f6b3e
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-07-21 20:39:34 +0700

    update

--- src/controller/Auth.controller.ts
+++ src/controller/Auth.controller.ts
@@ -24,14 +24,21 @@ export default class AuthController {
         throw new Error("Username already exist!");
       }
 
-      await Repository.Auth.signUp({
+      const createdUser = await Repository.Auth.signUp({
         username: lowerCaseUsername,
         password: hashedPassword,
       });
 
+      const user = {
+        username: lowerCaseUsername,
+        id: createdUser._id,
+      };
+
+      const token = createToken(lowerCaseUsername);
+
       return res
         .status(200)
-        .json({ data: null, message: "Success create Account!" });
+        .json({ data: { token, user }, message: "Success create Account!" });
     } catch (error: any) {
       console.log("error signup: ", error.message);
 
@@ -67,9 +74,10 @@ export default class AuthController {
 
       const { username: payloadUsername, _id: id } = usernameExist;
 
-      return res
-        .status(200)
-        .json({ data: { token, user: { id, username: payloadUsername } } });
+      return res.status(200).json({
+        data: { token, user: { id, username: payloadUsername } },
+        message: "Success signin!",
+      });
     } catch (error: any) {
       console.log("error signup: ", error.message);
 
